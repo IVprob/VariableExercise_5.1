4aca0265e6e149b3a6868cc1e528fbb164251a84
IVProb <jnoojinjr@gmail.com>
2021-03-25 23:26:07 -0500

    initial commit

---
+++ VariableExercise_5.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VariableExercise_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName;
+            int dogAge;
+            bool dogYears;
+            string dogYorN;
+
+            Console.WriteLine("What is your dog's name?");
+            dogName = Console.ReadLine();
+
+            Console.WriteLine($"How old is {dogName} in human years?");
+            dogAge = int.Parse(Console.ReadLine()); //this line will take string input from user and convert it to a int
+
+            //how to convert the input of yes or no to True or False? 
+            Console.WriteLine($"Do you want to know how old {dogName} is in dog years? Yes or No?");
+            dogYorN = Console.ReadLine();
+
+            if (dogYorN == "yes") // how to account for other answers "Yes", "ye", etc
+            {
+                Convert.ToBoolean(true);
+                Console.WriteLine(7 * dogAge);
+                dogYears = true;
+            }
+            else
+            {
+                Convert.ToBoolean(false);
+                Console.WriteLine("What ev...");
+                dogYears = false;
+            }
+        }
+    }
+}
